356bd49f391004317e492e0b527c7088fd4c7132
wonucode <lww@nene.studio>
2026-07-15 19:48:09 +0900

    chore: AI 협업 사운드 확장용 Master Audio 패키지 추가

---
+++ Assets/Plugins/DarkTonic/MasterAudio/Scripts/Settings/CustomEventCategory.cs
@@ -0,0 +1,60 @@
+﻿/*! \cond PRIVATE */
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+// ReSharper disable once CheckNamespace
+namespace DarkTonic.MasterAudio {
+	[Serializable]
+	// ReSharper disable once CheckNamespace
+	public class CustomEventCategory {
+		public string CatName = MasterAudio.NoCategory;
+		public bool IsExpanded = true;
+		public bool IsEditing = false;
+		public bool IsTemporary = false;
+		public string ProspectiveName = MasterAudio.NoCategory;
+
+#if UNITY_6000_4_OR_NEWER
+        private readonly List<EntityId> _actorInstanceIds = new List<EntityId>();
+#else
+        private readonly List<int> _actorInstanceIds = new List<int>();
+#endif
+
+
+#if UNITY_6000_4_OR_NEWER
+        public void AddActorInstanceId(EntityId instanceId) {
+            if (_actorInstanceIds.Contains(instanceId)) {
+                return;
+            }
+
+            _actorInstanceIds.Add(instanceId);
+        }
+
+        public void RemoveActorInstanceId(EntityId instanceId) {
+            _actorInstanceIds.Remove(instanceId);
+        }
+#else
+        public void AddActorInstanceId(int instanceId)
+        {
+            if (_actorInstanceIds.Contains(instanceId))
+            {
+                return;
+            }
+
+            _actorInstanceIds.Add(instanceId);
+        }
+
+        public void RemoveActorInstanceId(int instanceId)
+        {
+            _actorInstanceIds.Remove(instanceId);
+        }
+#endif
+
+        public bool HasLiveActors {
+            get {
+                return _actorInstanceIds.Count > 0;
+            }
+        }
+    }
+}
+/*! \endcond */
---
+++ Assets/Plugins/DarkTonic/MasterAudio/Scripts/Singleton/AudioClipOptimizer.cs
@@ -0,0 +1,31 @@
+﻿/*! \cond PRIVATE */
+
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class AudioClipOptimizer {
+#if UNITY_6000_4_OR_NEWER
+    private static readonly Dictionary<EntityId, string> AudioClipNameByInstanceId = new Dictionary<EntityId, string>();
+#else
+    private static readonly Dictionary<int, string> AudioClipNameByInstanceId = new Dictionary<int, string>();
+#endif
+
+    public static string CachedName(this AudioClip clip)
+    {
+#if UNITY_6000_4_OR_NEWER
+        var instanceId = clip.GetEntityId();
+#else
+        var instanceId = clip.GetInstanceID();
+#endif
+        if (AudioClipNameByInstanceId.ContainsKey(instanceId))
+        {
+            return AudioClipNameByInstanceId[instanceId];
+        }
+
+        var clipName = clip.name; // allocate
+        AudioClipNameByInstanceId.Add(instanceId, clipName);
+
+        return clipName;
+    }
+}
+/*! \endcond */
